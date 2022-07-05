a9ef4d47a077321e7af30bf137ea7331f48552d9
ashram7 <ashram7@hanmail.net>
2022-07-05 09:58:04 +0900

    Array

--- chap04/src/sec03/ContinueExample.java
+++ chap04/src/sec03/ContinueExample.java
@@ -4,7 +4,7 @@ public class ContinueExample {
 
 	public static void main(String[] args) {
 		for(int i=1; i<=10; i++) {
-			if(i%2 != 1) {
+			if(i%2 != 0) {
 				continue;
 			}
 			System.out.println(i);
